19111ca47b28c0f269785bb040cbeb50cb0910f1
ronny <rhanez07@gmail.com>
2026-07-09 00:45:49 -0400

    configurar url de produccion

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
   production: true,
-  // ⚠️ Reemplaza esta URL con la URL de tu backend en Render.com
-  apiUrl: 'https://maneja-tus-gastos.onrender.com',
+  apiUrl: 'https://version-alumnos.onrender.com/api',
 };
-
